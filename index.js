fe03b603971cf275f94d31054a8432d103ac1b09
Suvadeep <suvadeepm0709@gmail.com>
2025-05-27 22:46:41 +0530

    Add Stack intro questions

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-var checkIfPalindrome = function (s) {
-  let left = 0;
-  let right = s.length - 1;
-  while (left < right) {
-    if (s[left] !== s[right]) {
-      return false;
-    }
-    left++;
-    right--;
-  }
-  return true;
-};
+//declaration
+let stack = [];
 
-console.log(checkIfPalindrome("racecar"));
+//Pushing Elements
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+//Popping elements
+stack.pop(); //3
+stack.pop(); //2
+
+//Check if empty
+!stack.length;
+
+//check element at top
+stack[stack.length - 1];
+
+//get size
+stack.length;
---
+++ licc/Stacks-Queues/02-string-problems.md
@@ -0,0 +1,8 @@
+## String Problems Using Stack
+
+String problems that can utilize a stack will involve iterating over the string and putting characters into the stack and comparing the top of the stack with the current characters at each iteration 
+
+**Problem 1** [20. Valid Parentheses](https://leetcode.com/problems/valid-parentheses/)
+
+**Solution**: 
+
