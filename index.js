34d92f5c58e55455b5f5c32df32427f20d782b0c
Suvadeep <suvadeepm0709@gmail.com>
2025-06-09 21:52:14 +0530

    Two Sum in sorted Array

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-var checkIfPalindrome = function (s) {
+var checkForTarget = function (nums, target) {
   let left = 0;
-  let right = s.length - 1;
+  let right = nums.length - 1;
   while (left < right) {
-    if (s[left] !== s[right]) {
-      return false;
-    }
-    left++;
-    right--;
+    let currSum = nums[left] + nums[right];
+    if (currSum > target) right--;
+    else if (currSum < target) left++;
+    else return true;
   }
-  return true;
+  return false;
 };
+
+let arr = [1, 2, 4, 6, 8, 9, 14, 15];
+let target = 13;
+console.log(checkForTarget(arr, target));
