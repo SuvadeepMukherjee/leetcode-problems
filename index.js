b79839d931b45133a8fdaa8d4fc4522c135192de
suva <suvadeepm0709@gmail.com>
2025-06-21 22:24:55 +0530

    Add notes

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-class ListNode {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
+//head  is  the head node of a LL
+function fn(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    //Do something here
+    slow = slow.next;
+    fast = fast.next.next;
   }
 }
-
-let addNode = (prevNode, nodeToAdd) => {
-  nodeToAdd.next = prevNode.next;
-  prevNode.next = nodeToAdd;
-};
