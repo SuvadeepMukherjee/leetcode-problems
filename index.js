142105463b6386f204036324d8db2508dc2ff388
suva <suvadeepm0709@gmail.com>
2025-06-21 23:08:18 +0530

    slow and fast pointer

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-//head  is  the head node of a LL
-function fn(head) {
+let findNode = (head, k) => {
   let slow = head;
   let fast = head;
-  while (fast && fast.next) {
-    //Do something here
-    slow = slow.next;
-    fast = fast.next.next;
+  for (let i = 0; i < k; i++) {
+    fast = fast.next;
   }
-}
+
+  while (fast) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  return slow;
+};
