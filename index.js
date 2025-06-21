cd4d381cb63506ec486ca8b337a22362bdb1a367
suva <suvadeepm0709@gmail.com>
2025-06-21 21:12:52 +0530

    Add Linked List Questions

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-var findLength = function (s) {
-  let curr = 0;
-  let max = 0;
-  let left = 0;
-  for (let right = 0; right < s.length; right++) {
-    if (s[right] === "0") curr++;
-    while (curr > 1) {
-      if (s[left] === "0") {
-        curr -= 1;
-      }
-      left++;
-    }
-    max = Math.max(max, right - left + 1);
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
-  return max;
-};
+}
 
-console.log(findLength("1101100111"));
+let addNode = (prevNode, nodeToAdd) => {
+  nodeToAdd.next = prevNode.next;
+  prevNode.next = nodeToAdd;
+};
